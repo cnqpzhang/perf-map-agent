14a04b430fb1be1e9c9fd5c4c919d2b96267baa3
Patrick Zhang <patrick.zhang@amperecomputing.com>
2021-03-15 14:04:57 +0800

    check if JAVA_HOME got configured

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "x${JAVA_HOME}" = "x" ] || [ ! -f ${JAVA_HOME}/bin/java ]; then
+  echo "JAVA_HOME is not a valid path: ($JAVA_HOME)"
+  exit 1
+fi
+
 rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile out >/dev/null 2>&1
 cmake .
 make
